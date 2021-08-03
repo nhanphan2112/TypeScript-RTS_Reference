7f3b274f2db4af1642e727c98dd4b66d13c1d47e
Nhan Phan <nphanacc@gmail.com>
2021-08-02 19:13:24 -0700

    Explicit Component Type Annotation

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -3,9 +3,17 @@ interface ChildProps {
 }
 
 export const Child = ({ color }: ChildProps) => {
-	return (
-		<div>
-			<h1>{color}</h1>
-		</div>
-	);
+	return <div>{color}</div>;
 };
+
+//Explicit Component Type Annotation
+// ChildAsFC will be a React function component
+// Child might have properties assigned to it like 'propTypes'
+// 'Child' will receive props of type 'ChildProps'
+export const ChildAsFC: React.FunctionComponent<ChildProps> = ({ color }) => {
+	return <div>{color}</div>;
+};
+
+// example
+ChildAsFC.displayName;
+// Child.displayName; will show error because you did not define Chile as a React function component
