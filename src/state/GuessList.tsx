04aa52370dd536506e5b870e4dd5abc8cfa1e494
Nhan Phan <nphanacc@gmail.com>
2021-08-04 21:22:49 -0700

    11-Type Inference With State

--- src/state/GuessList.tsx
+++ src/state/GuessList.tsx
@@ -3,12 +3,18 @@ import {useState} from 'react';
 
 const GuessList: React.FC = () => {
   const [name, setName] = useState('');
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () =>{
+    setName('');
+    setGuests([...guests, name]);
+  }
 
 	return (
 		<div>
-			<h3>Guess List</h3>
+			<h3>Guest List</h3>
       <input value={name} onChange={(e)=>setName(e.target.value)}/>
-      <button>Add Guess</button>
+      <button onClick={onClick}>Add Guess</button>
 		</div>
 	);
 };
