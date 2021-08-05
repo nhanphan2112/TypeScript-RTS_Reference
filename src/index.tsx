11e6f1681137bf6e18a07a31971028a3fd402b8b
Nhan Phan <nphanacc@gmail.com>
2021-08-04 21:59:41 -0700

    16-Type for Standalone Event Handler that got define outside of jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import GuessList from "./state/GuessList";
 import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
 	return (
 		// <GuessList/>
-    <UserSearch/>
+		// <UserSearch/>
+		<EventComponent />
 	);
 };
 
