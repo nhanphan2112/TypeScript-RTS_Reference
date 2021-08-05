42abb9d324388a8b22d04f36b218992ee4353c89
Nhan Phan <nphanacc@gmail.com>
2021-08-05 09:43:09 -0700

    18-Typescript with Class Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import ReactDOM from "react-dom";
 import GuessList from "./state/GuessList";
-import UserSearch from "./state/UserSearch";
+// import UserSearch from "./state/UserSearch";
+import UserSearch from "./classes/UserSearch";
 import EventComponent from "./events/EventComponent";
+
+
+//Below are users objec array use to test for class UserSearch component
+const users = [
+	{ name: "Sarah", age: 20 },
+	{ name: "Alex", age: 25 },
+	{ name: "Michael", age: 27 },
+];
 
 const App = () => {
 	return (
 		// <GuessList/>
-		// <UserSearch/>
-		<EventComponent />
+		<UserSearch users={users} />
+		// <EventComponent />
 	);
 };
 
